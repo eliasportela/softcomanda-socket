88874790cd0f7d0c6c5f8b4de2f871e783260c09
Elias Douglas <edwportela@gmail.com>
2019-05-24 15:35:17 -0300

    Namespace

--- server.js
+++ server.js
@@ -2,30 +2,76 @@ var socket = require( 'socket.io' );
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
 var io = socket.listen( server );
+
 var port = process.env.PORT || 3000;
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
-io.on('connection', function (socket) {
+
+// app.get('/class/:classID', function(req,res) {
+//   className = req.params.classID;
+//   nsp = io.of('/' + className)
+//   res.sendFile(__dirname + '/index.html');
+
+//   nsp.on('connection', function(socket) {
+    
+//     socket.on('new_order', function(data) {
+//         e1.emit('new_order', {
+//             data: data
+//         });
+//     });
+
+//   });
+// });
+
+var e1 = io.of('/1');
+e1.on('connection', function (socket) {
     
     //novo pedido
     socket.on('new_order', function(data) {
-        io.sockets.emit('new_order', {
+        e1.emit('new_order', {
             data: data
         });
     });
     
     //nova comanda
     socket.on('new_menu', function(data) {
-        io.sockets.emit('new_menu', {
+        e1.emit('new_menu', {
             data: data
         });
     });
 
     //pedido finalizado
     socket.on('order_finished', function(data) {
-        io.sockets.emit('order_finished', {
+        e1.emit('order_finished', {
+            data: data
+        });
+    });
+    
+});
+
+var e2 = io.of('/2');
+e2.on('connection', function (socket) {
+    
+    //novo pedido
+    socket.on('new_order', function(data) {
+    	e2.emit('new_order', {
+        	data: data
+    	});
+    });
+    
+    //nova comanda
+    socket.on('new_menu', function(data) {
+        e2.emit('new_menu', {
+            data: data
+        });
+    });
+
+    //pedido finalizado
+    socket.on('order_finished', function(data) {
+        e2.emit('order_finished', {
             data: data
         });
     });
