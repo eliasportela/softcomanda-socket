912144e49396344a115b6e952f7f587fbbc52069
Elias Douglas Wenceslau Portela <edwportela@gmail.com>
2024-02-26 14:34:08 -0300

    new_chat

--- server.js
+++ server.js
@@ -168,6 +168,16 @@ app.post('/api/request-human', function(req, res) {
   res.json({success: true});
 });
 
+app.post('/api/new-chat', function(req, res) {
+  const { socket_id, message } = req.body;
+
+  if (socket_id) {
+    ioempresas.in(socket_id).emit('new_chat', { message });
+  }
+
+  res.json({success: true});
+});
+
 app.get('/api/list-online', function(req, res) {
   res.json({empresas: Object.keys(ioempresas.adapter.rooms)});
 });
